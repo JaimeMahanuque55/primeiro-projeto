5430f570d4a262db5b4e27a8f8e0b9961208a1db
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-08-17 16:45:40 +0100

    selec dados 2

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -24,20 +24,45 @@
 
 ////////////// CRIANDO UMA API DINAMICA ////////////
 
-import { NextApiHandler } from "next";
-import { Users } from "../../../utils/users";
+// import { NextApiHandler } from "next";
+// import { Users } from "../../../utils/users";
 
-const handler: NextApiHandler = (req, res) => {
-  console.log(req.query)
+// const handler: NextApiHandler = (req, res) => {
+//   console.log(req.query)
+//   const { id } = req.query;
+
+//   for (let i in Users) {
+//     if (Users[i].id.toString() === id) {
+//       res.json(Users[i]);
+//       return;
+//     }
+//   }
+
+//   res.json({ error: 'User not Found' });
+// }
+
+// export default handler;
+
+////////////// PRISMA SELECIONANDO DADOS 2 ///////////////////
+
+
+import { NextApiHandler } from "next";
+import prisma from "../../../libs/prisma";
+
+const handler: NextApiHandler = async (req, res) => {
   const { id } = req.query;
 
-  for (let i in Users) {
-    if (Users[i].id.toString() === id) {
-      res.json(Users[i]);
-      return;
+  const user = await prisma.user.findFirst({
+    where: {
+      id: parseInt(id as string),
+      active: true
     }
-  }
+  });
 
+  if (user) {
+    res.json({ status: true, user: user });
+    return;
+  }
   res.json({ error: 'User not Found' });
 }
 
