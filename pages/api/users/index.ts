2af86c33d0049dc73af339e27db269c3016c1295
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-08-17 16:28:44 +0100

    selec dados 1

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -138,17 +138,66 @@
 
 //////////////// PRISMA CLIENT: INSERINDO DADOS ///////////////
 
+// import { NextApiHandler } from "next";
+// import { Users } from "../../../utils/users";
+// import prisma from "../../../libs/prisma";
+
+// // Getting all users
+// const handlerGet: NextApiHandler = async (req, res) => {
+//   const { search, age } = req.query;
+//   res.status(200).json(Users);
+// }
+
+// // Inserting new user
+// const handlerPost: NextApiHandler = async (req, res) => {
+//   const { name, email } = req.body;
+
+//   const newUser = await prisma.user.create({
+//     data: {
+//       name: name,
+//       email: email,
+//     }
+//   });
+
+//   res.status(201).json({ status: true, user: newUser });
+
+// }
+
+
+// const handler: NextApiHandler = (req, res) => {
+
+//   switch (req.method) {
+//     case 'GET':
+//       handlerGet(req, res);
+//       break;
+//     case 'POST':
+//       handlerPost(req, res);
+//       break;
+
+//   }
+
+// }
+
+// export default handler;
+
+///////////////// PRISMA: SELECIONANDO DADOS //////////////////////
+
 import { NextApiHandler } from "next";
 import { Users } from "../../../utils/users";
 import prisma from "../../../libs/prisma";
 
 // Getting all users
 const handlerGet: NextApiHandler = async (req, res) => {
-  const { search, age } = req.query;
-  res.status(200).json(Users);
+  const users = await prisma.user.findMany({
+    where: {
+      active: true
+    }
+  });
+
+  res.json({ status: true, users });
 }
 
-// Inserting new user
+// Inserting new user 
 const handlerPost: NextApiHandler = async (req, res) => {
   const { name, email } = req.body;
 
