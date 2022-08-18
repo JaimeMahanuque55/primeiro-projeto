f358644fafef9decd0be83bacbea41751289bee9
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-08-18 18:57:32 +0100

    deletando dados

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -132,6 +132,85 @@
 
 //////////////// PRISMA: ALTERANDO DADOS 2 /////////////////////
 
+// import { NextApiHandler } from "next";
+// import prisma from "../../../libs/prisma";
+
+// // Reading user info
+// const handlerGet: NextApiHandler = async (req, res) => {
+//   const { id } = req.query;
+
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       id: parseInt(id as string),
+//       active: true
+//     }
+//   });
+
+//   if (user) {
+//     res.json({ status: true, user: user });
+//     return;
+//   }
+//   res.json({ error: 'User not Found' });
+// }
+
+// const handlerPut: NextApiHandler = async (req, res) => {
+//   const { name, active } = req.body;
+//   const { id } = req.query;
+
+//   let data: {
+//     name?: string;
+//     active?: boolean;
+//   } = {};
+
+//   if (name) {
+//     data.name = name;
+//   }
+//   if (active) {
+//     switch (active) {
+//       case 'true':
+//       case '1':
+//         data.active = true;
+//         break;
+//       case 'false':
+//       case '0':
+//         data.active = false;
+//         break;
+//     }
+//   }
+
+//   const updatedUser = await prisma.user.update({
+//     where: {
+//       id: parseInt(id as string)
+//     },
+//     data: data
+//   });
+
+//   if (updatedUser) {
+//     res.json({ status: true, user: updatedUser });
+//     return;
+//   }
+
+//   res.json({ error: 'Nao foi possivel alterar este usuario.' });
+
+// }
+
+
+// const handler: NextApiHandler = async (req, res) => {
+//   switch (req.method) {
+//     case 'GET':
+//       handlerGet(req, res);
+//       break;
+
+//     case 'PUT':
+//       handlerPut(req, res);
+//       break;
+//   }
+// }
+
+// export default handler;
+
+//////////// PRISMA: DELETANDO DADOS /////////////////
+
 import { NextApiHandler } from "next";
 import prisma from "../../../libs/prisma";
 
@@ -194,6 +273,22 @@ const handlerPut: NextApiHandler = async (req, res) => {
 
 }
 
+const handlerDelete: NextApiHandler = async (req, res) => {
+  const { id } = req.query;
+
+  const deletedUser = await prisma.user.delete({
+    where: {
+      id: parseInt(id as string)
+    }
+  }).catch(() => {
+    res.json({ error: 'Usuario nao encontrado' })
+  })
+
+  if (deletedUser) {
+    res.json({ status: true });
+  }
+}
+
 
 const handler: NextApiHandler = async (req, res) => {
   switch (req.method) {
@@ -203,6 +298,10 @@ const handler: NextApiHandler = async (req, res) => {
 
     case 'PUT':
       handlerPut(req, res);
+      break;
+
+    case 'DELETE':
+      handlerDelete(req, res);
       break;
   }
 }
