1f69d4e5c81e00627400d6ac9c791e1a388f8670
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-08-18 19:40:52 +0100

    Prisma: Filtro de Dados

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -231,6 +231,59 @@
 
 ///////////// PRISMA: SELECIONANDO CAMPOS ///////////////////////
 
+// import { NextApiHandler } from "next";
+// import { Users } from "../../../utils/users";
+// import prisma from "../../../libs/prisma";
+
+// // Getting all users
+// const handlerGet: NextApiHandler = async (req, res) => {
+//   const users = await prisma.user.findMany({
+//     where: {
+//       active: true
+//     },
+//     select: {
+//       id: true,
+//       name: true,
+//       email: true
+//     }
+//   });
+//   res.json({ status: true, users });
+// }
+
+// // Inserting new user 
+// const handlerPost: NextApiHandler = async (req, res) => {
+//   const { name, email } = req.body;
+
+//   const newUser = await prisma.user.create({
+//     data: {
+//       name: name,
+//       email: email,
+//     }
+//   });
+
+//   res.status(201).json({ status: true, user: newUser });
+
+// }
+
+
+// const handler: NextApiHandler = (req, res) => {
+
+//   switch (req.method) {
+//     case 'GET':
+//       handlerGet(req, res);
+//       break;
+//     case 'POST':
+//       handlerPost(req, res);
+//       break;
+
+//   }
+
+// }
+
+// export default handler;
+
+/////////////// PRISMA: FILTRO DE DADOS /////////////////
+
 import { NextApiHandler } from "next";
 import { Users } from "../../../utils/users";
 import prisma from "../../../libs/prisma";
@@ -239,7 +292,15 @@ import prisma from "../../../libs/prisma";
 const handlerGet: NextApiHandler = async (req, res) => {
   const users = await prisma.user.findMany({
     where: {
-      active: true
+      // name: 'Assuntinha'
+      // name: {
+      //   // startsWith: 'A',
+      //   // endsWith: 'a'
+      // }
+      OR: [
+        { name: 'Assuntinha' },
+        { name: { startsWith: 'J' } }
+      ]
     },
     select: {
       id: true,
