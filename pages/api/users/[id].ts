18e11eda638d63740c8bdb1e42d19279df09c5bc
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-08-18 18:40:04 +0100

    alter dados 2

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -71,6 +71,67 @@
 
 ///////////// PRISMA:ALTERANDO DADOS 1 /////////////////
 
+// import { NextApiHandler } from "next";
+// import prisma from "../../../libs/prisma";
+
+// // Reading user info
+// const handlerGet: NextApiHandler = async (req, res) => {
+//   const { id } = req.query;
+
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       id: parseInt(id as string),
+//       active: true
+//     }
+//   });
+
+//   if (user) {
+//     res.json({ status: true, user: user });
+//     return;
+//   }
+//   res.json({ error: 'User not Found' });
+// }
+
+// const handlerPut: NextApiHandler = async (req, res) => {
+//   const { name, active } = req.body;
+//   const { id } = req.query;
+
+//   const updatedUser = await prisma.user.update({
+//     where: {
+//       id: parseInt(id as string)
+//     },
+//     data: {
+//       name: name,
+//       active: active === 'true' ? true : false
+//     }
+//   });
+
+//   if (updatedUser) {
+//     res.json({ status: true, user: updatedUser });
+//     return;
+//   }
+
+//   res.json({ error: 'Nao foi possivel alterar este usuario.' });
+
+// }
+
+
+// const handler: NextApiHandler = async (req, res) => {
+//   switch (req.method) {
+//     case 'GET':
+//       handlerGet(req, res);
+//       break;
+
+//     case 'PUT':
+//       handlerPut(req, res);
+//       break;
+//   }
+// }
+
+// export default handler;
+
+//////////////// PRISMA: ALTERANDO DADOS 2 /////////////////////
+
 import { NextApiHandler } from "next";
 import prisma from "../../../libs/prisma";
 
@@ -96,14 +157,32 @@ const handlerPut: NextApiHandler = async (req, res) => {
   const { name, active } = req.body;
   const { id } = req.query;
 
+  let data: {
+    name?: string;
+    active?: boolean;
+  } = {};
+
+  if (name) {
+    data.name = name;
+  }
+  if (active) {
+    switch (active) {
+      case 'true':
+      case '1':
+        data.active = true;
+        break;
+      case 'false':
+      case '0':
+        data.active = false;
+        break;
+    }
+  }
+
   const updatedUser = await prisma.user.update({
     where: {
       id: parseInt(id as string)
     },
-    data: {
-      name: name,
-      active: active === 'true' ? true : false
-    }
+    data: data
   });
 
   if (updatedUser) {
