4896c55e46e3bc9ededab02635eba2e9812e7318
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-05 14:25:11 +0100

    Usando a API no Server-side

--- pages/usuarios.tsx
+++ pages/usuarios.tsx
@@ -36,6 +36,54 @@
 
 ///////////////// APLICANDO O CONCEITO DRY NA API /////////////
 
+// import Head from "next/head";
+// import { Layout } from "../components/Layout";
+// import styles from '../styles/Usuarios.module.css';
+// import api from "../libs/api";
+// import { User } from "../types/User";
+
+// type Props = {
+//   users: User[];
+// }
+
+// const Usuarios = ({ users }: Props) => {
+
+//   return (
+//     <Layout>
+//       <div>
+//         <Head>
+//           <title>Usuarios</title>
+//         </Head>
+//         <h1>Pagina Usuarios</h1>
+//         <ul>
+//           {users.map((item, index) => (
+//             <li key={index}>{item.name}</li>
+//           ))}
+//         </ul>
+
+
+
+//       </div>
+//     </Layout>
+//   );
+// }
+
+// export const getServerSideProps = async () => {
+//   // DRY = Dont Repeat Yourself
+
+//   const users = await api.getAllUsers(0);
+//   return {
+//     props: {
+//       users
+//     }
+//   }
+// }
+
+
+// export default Usuarios;
+
+////////// Usando a api no server-side ///////////////
+
 import Head from "next/head";
 import { Layout } from "../components/Layout";
 import styles from '../styles/Usuarios.module.css';
@@ -72,6 +120,9 @@ export const getServerSideProps = async () => {
   // DRY = Dont Repeat Yourself
 
   const users = await api.getAllUsers(0);
+
+  const req = await fetch('https://jsonplaceholder.typicode.com/todos'); // quando quer usar uma api externa no server-side
+
   return {
     props: {
       users
