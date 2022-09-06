1b5e7403fcc342672f315b531369784eaaba27e0
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-06 14:07:37 +0100

    Login: Páginas privadas (via server-side)

--- pages/usuarios/index.tsx
+++ pages/usuarios/index.tsx
@@ -370,6 +370,101 @@
 
 ////////////// Login: Paginas privadas (via client-side) //////////
 
+// import Head from "next/head";
+// import { Layout } from "../../components/Layout";
+// import styles from '../styles/Usuarios.module.css';
+// import api from "../../libs/api";
+// import { User } from "../../types/User";
+// import { useState } from "react";
+// import Link from "next/link";
+// import axios from "axios";
+// import { useSession } from "next-auth/react";
+
+// type Props = {
+//   users: User[];
+// }
+
+// const Usuarios = ({ users }: Props) => {
+//   const { data: session, status: sessionStatus } = useSession();
+
+
+
+//   const [showMore, setShowMore] = useState(true);
+//   const [loading, setLoading] = useState(false);
+//   const [pageCount, setPageCount] = useState(1);
+//   const [userList, setUserList] = useState<User[]>(users);
+
+//   const handleLoadMore = async () => {
+
+//     if (!loading) {
+//       setLoading(true)
+//       const json = await axios.get(`/api/users?page=${pageCount + 1}`);
+//       if (json.data.status) {
+//         if (json.data.users.length === 0) {
+//           setShowMore(false);
+//         }
+//         setUserList([...userList, ...json.data.users]);
+//       }
+//       setLoading(false);
+//       setPageCount(pageCount + 1);
+//     }
+//   }
+
+//   return (
+//     <Layout>
+//       <div>
+//         <Head>
+//           <title>Usuarios</title>
+//         </Head>
+
+//         {sessionStatus === 'loading' &&
+//           <div>Carregando...</div>
+//         }
+//         {sessionStatus === 'unauthenticated' &&
+//           <div>Voce nao tem permissao para acessar este conteudo</div>
+//         }
+//         {sessionStatus === 'authenticated' &&
+//           <>
+//             <h1>Pagina Usuarios</h1>
+
+//             <Link href={`/usuarios/novo`}>Novo Usuario</Link>
+
+//             <ul>
+//               {userList.map((item, index) => (
+//                 <li key={index}>{item.name}</li>
+//               ))}
+//             </ul>
+//             {showMore &&
+//               <button onClick={handleLoadMore}>Carregar mais</button>
+//             }
+//           </>
+//         }
+
+
+
+
+//       </div>
+//     </Layout>
+//   );
+// }
+
+// export const getServerSideProps = async () => {
+//   // DRY = Dont Repeat Yourself
+
+//   const users = await api.getAllUsers(0);
+
+
+//   return {
+//     props: {
+//       users
+//     }
+//   }
+// }
+
+// export default Usuarios;
+
+//////////// Login: Paginas privadas (via server-side) //////////
+
 import Head from "next/head";
 import { Layout } from "../../components/Layout";
 import styles from '../styles/Usuarios.module.css';
@@ -378,15 +473,17 @@ import { User } from "../../types/User";
 import { useState } from "react";
 import Link from "next/link";
 import axios from "axios";
-import { useSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { AuthUser } from "../../types/AuthUser";
 
 type Props = {
   users: User[];
+  loggedUser: AuthUser;
 }
 
-const Usuarios = ({ users }: Props) => {
-  const { data: session, status: sessionStatus } = useSession();
-
+const Usuarios = ({ users, loggedUser }: Props) => {
 
 
   const [showMore, setShowMore] = useState(true);
@@ -417,45 +514,43 @@ const Usuarios = ({ users }: Props) => {
           <title>Usuarios</title>
         </Head>
 
-        {sessionStatus === 'loading' &&
-          <div>Carregando...</div>
+        <h1>Pagina Usuarios</h1>
+        <div>Ola {loggedUser.name}. (Tipo: {loggedUser.role})</div>
+
+        <Link href={`/usuarios/novo`}>Novo Usuario</Link>
+
+        <ul>
+          {userList.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
+        {showMore &&
+          <button onClick={handleLoadMore}>Carregar mais</button>
         }
-        {sessionStatus === 'unauthenticated' &&
-          <div>Voce nao tem permissao para acessar este conteudo</div>
-        }
-        {sessionStatus === 'authenticated' &&
-          <>
-            <h1>Pagina Usuarios</h1>
-
-            <Link href={`/usuarios/novo`}>Novo Usuario</Link>
-
-            <ul>
-              {userList.map((item, index) => (
-                <li key={index}>{item.name}</li>
-              ))}
-            </ul>
-            {showMore &&
-              <button onClick={handleLoadMore}>Carregar mais</button>
-            }
-          </>
-        }
-
-
-
-
       </div>
     </Layout>
   );
 }
 
-export const getServerSideProps = async () => {
-  // DRY = Dont Repeat Yourself
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req, context.res, authOptions
+  );
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: true
+      }
+    }
+  }
 
   const users = await api.getAllUsers(0);
 
 
   return {
     props: {
+      loggedUser: session.user,
       users
     }
   }
