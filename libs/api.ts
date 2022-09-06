903c50745738fd12c85d7dbc761861cc30b840bf
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-06 10:53:26 +0100

    Login: Autorizando via Prisma

--- libs/api.ts
+++ libs/api.ts
@@ -1,5 +1,90 @@
 ///////////////////// APLICANDO O CONCEITO DRY NA API //////////////
 
+// import prisma from "./prisma";
+
+// export default {
+//   getAllUsers: async (page: number) => {
+//     // Items per page
+//     let take = 3;
+
+//     // offset of items
+//     let skip = 0;
+//     if (page) {
+//       skip = (page - 1) * take;
+//     }
+
+
+//     const users = await prisma.user.findMany({
+//       skip: skip, //Pula os primeiros itens
+//       take: take, // Pega os itens seguintes
+//       where: {
+//         active: true
+//       },
+//       select: {
+//         id: true,
+//         name: true,
+//         email: true
+//       },
+//       orderBy: [
+//         { id: 'asc' }
+//       ]
+//     });
+//     return users;
+//   },
+//   addUser: async (name: string, email: string) => {
+//     return await prisma.user.create({
+//       data: { name, email }
+//     });
+//   },
+//   getUser: async (id: number) => {
+//     const user = await prisma.user.findFirst({
+//       where: {
+//         id,
+//         active: true
+//       }
+//     });
+//     return user;
+//   },
+//   updateUser: async (id: number, name?: string, active?: string) => {
+//     let data: {
+//       name?: string;
+//       active?: boolean;
+//     } = {};
+
+//     if (name) {
+//       data.name = name;
+//     }
+//     if (active) {
+//       switch (active) {
+//         case 'true':
+//         case '1':
+//           data.active = true;
+//           break;
+//         case 'false':
+//         case '0':
+//           data.active = false;
+//           break;
+//       }
+//     }
+
+//     const updatedUser = await prisma.user.update({
+//       where: { id },
+//       data: data
+//     });
+//     return updatedUser;
+//   },
+//   deleteUser: async (id: number) => {
+//     return await prisma.user.delete({
+//       where: {
+//         id
+//       }
+//     })
+//   }
+
+// }
+
+///////// LOGIN: Autorizando via Prisma //////////////
+
 import prisma from "./prisma";
 
 export default {
@@ -36,12 +121,15 @@ export default {
       data: { name, email }
     });
   },
+  getUserFromEmail: async (email: string) => {
+    const user = await prisma.user.findFirst({
+      where: { email, active: true }
+    });
+    return user;
+  },
   getUser: async (id: number) => {
     const user = await prisma.user.findFirst({
-      where: {
-        id,
-        active: true
-      }
+      where: { id, active: true }
     });
     return user;
   },
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,8 +12,42 @@
 
 /////////// LOGIN: Usando o CredentialsProvider /////////////////
 
+// import NextAuth, { NextAuthOptions } from 'next-auth';
+// import CredentialsProvider from 'next-auth/providers/credentials';
+
+
+// export const authOptions: NextAuthOptions = {
+//   secret: process.env.NEXTAUTH_SECRET,
+//   providers: [
+//     CredentialsProvider({
+//       id: 'credentials', // pode usar o nome que quiser
+//       credentials: {
+//         email: { label: 'E-mail', type: 'text' },
+//         password: { label: 'Senha', type: 'password' }
+//       },
+//       authorize: async (credentials, req) => { // funcao responsavel por autorizar o login
+//         if (credentials?.email === 'james@gmail.com') {
+//           const user = {
+//             id: 123,
+//             name: 'James',
+//             email: 'james@gmail.com',
+//             role: 'USER'
+//           }
+//           return user;
+//         }
+//         return null;
+//       }
+//     })
+//   ]
+// }
+
+// export default NextAuth(authOptions);
+
+///////////// LOGIN: Autorizando via Prisma ////////////////
+
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import api from '../../../libs/api';
 
 
 export const authOptions: NextAuthOptions = {
@@ -26,15 +60,19 @@ export const authOptions: NextAuthOptions = {
         password: { label: 'Senha', type: 'password' }
       },
       authorize: async (credentials, req) => { // funcao responsavel por autorizar o login
-        if (credentials?.email === 'james@gmail.com') {
-          const user = {
-            id: 123,
-            name: 'James',
-            email: 'james@gmail.com',
-            role: 'USER'
+        if (credentials && credentials.email && credentials.password) {
+          const user = await api.getUserFromEmail(credentials.email);
+          if (user) {
+
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role
+            }
           }
-          return user;
         }
+
         return null;
       }
     })
