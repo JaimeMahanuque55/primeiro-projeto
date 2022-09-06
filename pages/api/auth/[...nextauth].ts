2d2b65542e85e5bd6fbd9edc7a27d0c44dc241d7
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-06 13:24:13 +0100

    Login: Resolvendo problema dos dados 2

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -45,9 +45,49 @@
 
 ///////////// LOGIN: Autorizando via Prisma ////////////////
 
+// import NextAuth, { NextAuthOptions } from 'next-auth';
+// import CredentialsProvider from 'next-auth/providers/credentials';
+// import api from '../../../libs/api';
+
+
+// export const authOptions: NextAuthOptions = {
+//   secret: process.env.NEXTAUTH_SECRET,
+//   providers: [
+//     CredentialsProvider({
+//       id: 'credentials', // pode usar o nome que quiser
+//       credentials: {
+//         email: { label: 'E-mail', type: 'text' },
+//         password: { label: 'Senha', type: 'password' }
+//       },
+//       authorize: async (credentials, req) => { // funcao responsavel por autorizar o login
+//         if (credentials && credentials.email && credentials.password) {
+//           const user = await api.getUserFromEmail(credentials.email);
+//           if (user) {
+
+//             return {
+//               id: user.id,
+//               name: user.name,
+//               email: user.email,
+//               role: user.role
+//             }
+//           }
+//         }
+
+//         return null;
+//       }
+//     })
+//   ]
+// }
+
+// export default NextAuth(authOptions);
+
+/////////// Login: Resolvendo problema dos dados 2 ////////////
+
+
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import api from '../../../libs/api';
+import { AuthUser } from '../../../types/AuthUser';
 
 
 export const authOptions: NextAuthOptions = {
@@ -76,7 +116,21 @@ export const authOptions: NextAuthOptions = {
         return null;
       }
     })
-  ]
+  ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user = token.user as AuthUser;
+      }
+      return session;
+    }
+  }
 }
 
 export default NextAuth(authOptions);
