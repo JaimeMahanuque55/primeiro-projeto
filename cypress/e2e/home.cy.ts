8e9bf8de7fcb5478ac6405ed416adae9d4b08d80
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-13 16:15:48 +0100

    Criando primeiros testes e2e com Cypress 2

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,10 +4,19 @@ describe('Home', () => {
   });
   it('should load the page in english', () => {
     cy.visit('/');
-    cy.get('header').should('contain', 'Chosen Language: en');
+    cy.get('header').should('contain', 'Choosen Language: en');
   });
   it('should load the page in wrong language', () => {
     cy.visit('/pt');
-    cy.get('header').should('not.contain', 'Chosen Language: en');
+    cy.get('header').should('not.contain', 'Choosen Language: en');
   });
+  it('should load the page in portuguese', () => {
+    cy.visit('/pt');
+    cy.get('header').should('contain', 'Linguagem Escolhida: pt');
+  });
+  it('should go to login page', () => {
+    cy.visit('/');
+    cy.contains('Do Login').click();
+    cy.url().should('include', '/api/auth/signin');
+  })
 });
